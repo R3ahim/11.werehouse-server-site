6e487fe3947552245eacd4d35d5e20d39a6bb5b2
itabdur <itabdurrahim22@gmail.com>
2022-05-08 13:50:58 +0600

    hero post

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Stoking and heroku  server start');
+    res.send('Stoking and herokuuu servers start');
 });
 
 
